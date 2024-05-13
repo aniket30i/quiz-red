b3962484c2e865d52f6490f75e66c6dcb3903d5c
aniket30i <iamaniket.saha.09@protonmail.com>
2024-05-13 22:29:20 +0530

    handling answers

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function Options({question}) {
+function Options({ question, dispatch, answer }) {
   return (
     <div className="options">
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>
-            {option}
-          </button>
-        ))}
-      </div>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""}`}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 }
 
-export default Options
+export default Options;
